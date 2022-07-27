16ac6e1923ba01a0d4fba56eeaf9795b406b64c9
Nóvoa González <inovoa@indra.es>
2022-07-27 12:51:04 +0200

    Ejercico accidentes

--- src/src/HelloWorld.java
+++ src/src/HelloWorld.java
@@ -1,88 +1,70 @@
+import java.io.File;
+import java.io.PrintWriter;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String weekday = getWeekday();
+        List<CarModel> carModels = new ArrayList<>();
+        carModels.add(new CarModel("Audi", "A1", 270));
+        carModels.add(new CarModel("Audi", "A2", 280));
+        carModels.add(new CarModel("Audi", "A3", 290));
+        carModels.add(new CarModel("Audi", "A5", 300));
+        carModels.add(new CarModel("Audi", "A8", 310));
+        carModels.add(new CarModel("Alfa Romeo", "AA", 300));
+        carModels.add(new CarModel("Alfa Romeo", "BB", 310));
+        carModels.add(new CarModel("Alfa Romeo", "CC", 300));
+        carModels.add(new CarModel("Ferrari", "Roja", 450));
+        carModels.add(new CarModel("Ford", "Mustang", 330));
+        carModels = carModels.stream().sorted(Comparator.comparing(CarModel::getBrand)).collect(Collectors.toList());
 
-        System.out.println("Hola, ¿como te llamas?");
-        String name = scanner.nextLine();
-        System.out.println("Hola, " + name + ", ¿como te sientes este bonito " + weekday + "?");
-        String stateMind = scanner.nextLine().toLowerCase(Locale.ROOT);
-        switch (stateMind) {
-            case "triste":
-                getJoke();
-                break;
-            case "alegre":
-                System.out.println("Me alegro");
-                break;
-            case "nervioso":
-                System.out.println("No te ralles");
-                break;
-            default:
-                System.out.println("No te he entendido");
-                break;
+        try {
+            PrintWriter printWriter = new PrintWriter(new File("cars.txt"));
+            carModels.forEach(printWriter::println);
+            printWriter.close();
+        } catch (Exception err) {
+            System.err.println(err.getMessage());
         }
     }
+}
 
-    private static String getWeekday() {
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTime(new Date());
-        switch (calendar.get(Calendar.DAY_OF_WEEK)) {
-            case 1: return "Lunes";
-            case 2: return "Martes";
-            case 3: return "Miércoles";
-            case 4: return "Jueves";
-            case 5: return "Viernes";
-            case 6: return "Sábado";
-            default: return "Domingo";
-        }
+class CarModel {
+    private String brand;
+    private String name;
+    private int maxSpeed;
+
+    public String getBrand() {
+        return brand;
     }
 
-    private static void getJoke() {
-        Random random = new Random();
-        int value = random.nextInt(10);
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
 
-        switch (value) {
-            case 0:
-                System.out.println("¿Cuál es el último animal que subió al arca de Noé? El del-fin.");
-                break;
+    public String getName() {
+        return name;
+    }
 
-            case 1:
-                System.out.println("¿Cómo se dice pañuelo en japonés? Saka-moko.");
-                break;
+    public void setName(String name) {
+        this.name = name;
+    }
 
-            case 2:
-                System.out.println("¿Cómo se dice disparo en árabe? Ahí-va-la-bala.");
-                break;
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
 
-            case 3:
-                System.out.println("¿Qué le dice un gusano a otro gusano? Voy a dar una vuelta a la manzana.");
-                break;
+    public void setMaxSpeed(int maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
 
-            case 4:
-                System.out.println("¿Cuál es el colmo de Aladdín? Tener mal genio.");
-                break;
+    public CarModel(String brand, String name, int maxSpeed) {
+        this.brand = brand;
+        this.name = name;
+        this.maxSpeed = maxSpeed;
+    }
 
-            case 5:
-                System.out.println("Si se muere una pulga, ¿a dónde va? Al pulgatorio.");
-                break;
-
-            case 6:
-                System.out.println("¿Cómo se dice pelo sucio en chino? Chin cham pu.");
-                break;
-
-            case 7:
-                System.out.println("Estás obsesionado con la comida, de verdad. ¿Qué dices croquetamente?");
-                break;
-
-            case 8:
-                System.out.println("¿Por qué las focas miran siempre hacia arriba? ¡Porque ahí están los focos!");
-                break;
-
-            case 9:
-                System.out.println("¿Cómo se llama el primo de Bruce Lee? Broco Lee.");
-                break;
-        }
+    @Override
+    public String toString() {
+        return "El model " + name + " de la marca " + brand + " tiene una velocidad máxima de " + maxSpeed;
     }
 }
